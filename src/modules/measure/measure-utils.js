665b67bc81736f73f85209d3033085e48406b17b
Mozart Diniz <mozart.diniz@gmail.com>
2018-02-12 20:58:38 +0100

    small changes

--- src/modules/measure/measure-utils.js
+++ src/modules/measure/measure-utils.js
@@ -1,12 +1,8 @@
 
-const MeasureUtils = {
-    calcImc(alt, peso){
-        if (alt && peso) {
-            return peso / Math.pow(alt / 100, 2);
+export const calcImc = (alt, peso) => {
+    if (alt && peso) {
+        return (peso / Math.pow(alt / 100, 2)).toFixed(2);
 
-        }
-        return '';
     }
-};
-
-export default MeasureUtils;
+    return '';
+}
